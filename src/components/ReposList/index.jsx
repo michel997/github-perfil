36a2589a7b4cad964199af885f0599c451082177
Michel <michellimamonteiro@hotmail.com>
2023-10-18 13:20:37 -0300

    primeiro commit

---
+++ src/components/ReposList/index.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+import styles from './ReposList.module.css'
+
+const ReposList = ({ nomeUsuario }) => {
+    const [repos, setRepos] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
+        .then(res => res.json())
+        .then(resJson => {
+            setRepos(resJson)
+        })
+    }, [nomeUsuario])
+
+    return(
+        <div className="container">
+            <ul className={styles.list}>
+            {repos.map(repositorio => (
+                <li className={styles.listItem} key={repositorio.id}>
+                    <div className={styles.itemName}>
+                        <b>Nome:</b> 
+                        {repositorio.name}
+                    </div>
+
+                    <div className={styles.itemLanguage}>
+                        <b>Linguagem:</b> 
+                        {repositorio.language}
+                    </div>
+
+                    <a className={styles.itemLink} target="_blank" href={repositorio.html_url}>Visitar no Github</a>
+                </li>
+            ))}
+        </ul>
+        </div>
+    )
+}
+
+export default ReposList;
